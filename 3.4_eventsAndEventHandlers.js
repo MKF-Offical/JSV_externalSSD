6329c78a713eb521dd59bda4bf277cef19a54f2c
MKF <kihongblue@gmail.com>
2021-06-09 19:04:28 +0900

    210609_1904

---
+++ 3.4_eventsAndEventHandlers.js
@@ -0,0 +1,14 @@
+function handleResize(){
+    console.log("I have been resized.")
+} ;
+
+window.addEventListener("resize", handleResize);
+
+const title = document.querySelector("#title");
+
+function handleClick() {
+    title.style.color="red"
+    console.log("Color changed!!")
+};
+
+title.addEventListener("click",handleClick);
